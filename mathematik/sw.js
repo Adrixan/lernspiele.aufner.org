24c6bec13022efdbc51dca33876c77dee521ff41
Peter Aufner <peter@code-alongsi.de>
2026-02-27 15:45:04 +0100

    Add Mathematik game and update Daz with new config structure

--- mathematik/sw.js
+++ mathematik/sw.js
@@ -5,7 +5,7 @@
  * Version: 1.0.0
  */
 
-const CACHE_NAME = 'mini-trainer-daz-v1772199272766';
+const CACHE_NAME = 'mini-trainer-mathematik-v1772199283859';
 const STATIC_CACHE_NAME = 'mini-trainer-static-v1';
 const DATA_CACHE_NAME = 'mini-trainer-data-v1';
 
