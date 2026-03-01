2605cd3841a07bcef2f2d4576e6ac20d708fcf46
Peter Aufner <peter@code-alongsi.de>
2026-03-01 18:16:38 +0100

    Fix in-memoriam-pascal

--- in-memoriam-pascal/registerSW.js
+++ in-memoriam-pascal/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/in-memoriam-pascal/sw.js', { scope: '/in-memoriam-pascal/' })})}
